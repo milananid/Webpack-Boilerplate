e397c357e1d8c3ae9a1f1532f8a7d7c9e6c18a21
milananid <33397026+milananid@users.noreply.github.com>
2018-04-26 13:20:10 +0200

    primo commit

---
+++ boilerplate/src/index.js
@@ -0,0 +1,10 @@
+import "smoothscroll-for-websites"
+import "./js/app"
+
+import "./scss/app.scss"
+
+import "./fonts/nome.woff2"
+import "./fonts/nome.woff"
+import "./fonts/nome.woff2"
+import "./img/nome.jpg"
+import "./public/nome.pdf"
---
+++ boilerplate/src/js/app.js
@@ -0,0 +1,18 @@
+/* global TweenMax TimelineMax Expo */
+import $ from "jquery"
+import "gsap"
+import "slick-carousel"
+import ScrollMagic from "scrollmagic"
+import "imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap"
+import imagesLoaded from "imagesloaded"
+import SplitText from "./SplitText"
+import "./MorphSVGPlugin"
+import animateScrollTo from "animated-scroll-to"
+
+const TOUCH_EVENT = "ontouchstart" in window ? "touchend" : "click"
+
+document.addEventListener("DOMContentLoaded", () => {
+	
+	//ready to go
+
+})
